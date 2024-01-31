4cadce93df3d0d5d369de79dfd0c3638e05d1445
neon <dogmanyo@gmail.com>
2024-01-31 16:09:47 -0500

    added comments to userRoutes.js

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,5 +1,7 @@
+// Import the express library
 const express = require('express');
 
+// Import the controllers
 const {
     getAllUsers,
     getUserById,
@@ -7,12 +9,20 @@ const {
     updateUser,
     deleteUser,
     addFriend,
-    deleteFriend,} = require('../../controllers/userController');
+    deleteFriend,
+} = require('../../controllers/userController');
 
+// Create a new router
 const router = express.Router();
 
+// Define the routes for getting all users and creating a new user
 router.route('/').get(getAllUsers).post(createUser);
+
+// Define the route for adding and deleting a friend for a specific user
 router.route('/:userId/friends/:friendId').post(addFriend).delete(deleteFriend);
+
+// Define the routes for getting, updating, and deleting a specific user
 router.route('/:userId').get(getUserById).put(updateUser).delete(deleteUser);
 
+// Export the router
 module.exports = router;
